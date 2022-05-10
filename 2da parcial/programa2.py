4d20bc1ea6b3cc9199ba8335422fbc65214b03d3
[Kmartinez24] <[kmartinez24@ucol.mx]>
2022-05-10 17:20:51 -0500

    Coso de cosa

--- 2da parcial/programa2.py
+++ 2da parcial/programa2.py
@@ -1,14 +1,11 @@
+# Kiyoshi Ali Martinez Renteria
 
 import nltk
-
-sentence =  "./Documentos/documento2.txt"
-tokens = nltk.word_tokenize(sentence)
-
-
-tagged = nltk.pos_tag(tokens)
-tagged[0:6]
-
-
 from nltk.corpus import treebank
+
+sentence = "./Documentos/documento2.txt"
+tokens = nltk.word_tokenize(sentence)
+print(tokens)
+
 t = treebank.parsed_sents('wsj_0001.mrg')[0]
 t.draw()
